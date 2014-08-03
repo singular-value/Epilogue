94e33defc4870e85741ed48e1ad10bc438bc2921
Stephen Cognetta <sycognetta@gmail.com>
2014-08-02 21:44:27 -0700

    database

--- Epilogue/epilogue.py
+++ Epilogue/epilogue.py
@@ -1,14 +1,15 @@
 import os
-import urllib
+import urllib, cgi
 
 import jinja2
 import webapp2
+from models import User
+me = User()
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
-
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -24,6 +25,7 @@ class LoginPage(webapp2.RequestHandler):
 
 class FuneralPage(webapp2.RequestHandler):
     def get(self):
+        #print 'FUCK YES THIS WORKS: ' + me.dead_name
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render({'message': "funeral"}))
 
@@ -45,6 +47,13 @@ class MembershipPage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render({'message': "membership"}))
 
+class Store(webapp2.RequestHandler):
+    def post(self):
+        print self.request
+        me.email = self.request.get('email')
+        me.dead_name = self.request.get('dead_name')
+        self.redirect('/funeral') #TAKE THEM TO THE NEXT PAGE HERE
+
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
@@ -53,4 +62,5 @@ application = webapp2.WSGIApplication([
     ('/social', SocialPage),
     ('/finance', FinancePage),
     ('/memberships', MembershipPage),
+    ('/store', Store)
 ], debug=True)
