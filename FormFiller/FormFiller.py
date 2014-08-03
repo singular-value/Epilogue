a2b7180295fdb93876638449dd4882bf13ebf123
Pranav Gokhale <pgokhale@princeton.edu>
2014-08-02 23:10:31 -0700

    FB memorialization iframe works, woot woot

--- FormFiller/FormFiller.py
+++ FormFiller/FormFiller.py
@@ -23,10 +23,38 @@ class MainPage(webapp2.RequestHandler):
         result = result.replace("<head>", """<head><base href="%s">""" % url)
         self.response.write(result)
 
-#class OtherPage(webapp2.RequestHandler):
-#    def get(self):
-#        htmlcode = 
+
+class LinkedInPage(webapp2.RequestHandler):
+    def get(self):
+        url = "static/linkedin.htm"
+        self.response.write("""blah""" % url)
+
+class FBPage(webapp2.RequestHandler):
+    def get(self):
+        url="static/fb.htm"
+        self.response.write("""<html>
+<head>
+<script>
+fill = function() {
+var iframe = document.getElementById('the_form');
+var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+form = innerDoc.forms[1]
+form["Field153786764731152"].value="%s"
+form["Field131943620266906"].value="%s"
+form["Field300731829979004"].value="%s"
+form["Field152395278213769"].value="%s"
+form["Field243993725684212"].value="%s"
+}
+</script>
+</head>
+<body><iframe id="the_form" width="1000px" height="800px" src="%s" onload="fill()"></iframe>
+</body></html>""" % ("Jessie Kris Lambert","https://www.facebook.com/bob.cardinal.50","bob.cardinal.epiloguetesting@gmail.com","epiloguesemail@pilog.ue","Immediate family ",url))
+#        url = "https://www.facebook.com/help/contact/305593649477238"
+#        result = urlfetch.fetch(url=url, method=urlfetch.GET)
+#        print result.content
+#        self.response.headers['Content-Type'] = 'text/plain'
+#        self.response.write(result.content.replace("<head>", """<head><base href="%s">""" % url))
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', MainPage), ('/fb', FBPage)
 ], debug=True)
